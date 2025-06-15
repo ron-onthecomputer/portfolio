611715cc8f8f5b639204286a767a85f75e52d298
Ron <69483157+ron-onthecomputer@users.noreply.github.com>
2025-06-15 17:54:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove",function(e){
+cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px;top: " +e.clientY + "px;";});  
   document.addEventListener("scroll", function () {
     const aboutSection = document.getElementById("about");
     const aboutContSection = document.getElementById("about-cont");
     const aboutLink = document.querySelector('a[href="#about"]');
-
     const triggerPoint = 150;
-
     const isInView = (elem) => {
       const rect = elem.getBoundingClientRect();
       return rect.top <= triggerPoint && rect.bottom > triggerPoint;
@@ -16,3 +18,16 @@
       aboutLink.classList.remove("active");
     }
   });
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon');
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+  if (hamburgerIcon.style.display === "none") {
+    hamburgerIcon.style.display = "inline";
+    closeIcon.style.display = "none";
+  } else {
+    hamburgerIcon.style.display = "none";
+    closeIcon.style.display = "inline";
+  }
+});
+  
